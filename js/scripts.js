0bf4a2195c372b1d383f9bfd8591d9cad32305b4
Abel L. MBULA <bam92@debian>
2017-05-14 09:17:10 +0200

    Refactoring using objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-var quiz = [
-	["What is Superman's real name?", "Clarke Kent"],
-	["What is Wonderwoman's real name?", "Dianna Prince"],
-	["What is Bataman's real name?", "Bruce Wayne"],
-];
+var quiz = {
+	"name":"Super Hero Neme Quiz",
+	"description":"How many heroes can you name?",
+	"question":"What is the real name of",
+	"questions": [
+		{ "question":"Superman", "answer":"Clarke Kent" },
+		{ "question":"Wonderwoman", "answer":"Dianna Prince" },
+		{ "question":"Bataman", "answer":"Bruce Wayne" }
+	]
+}
+
 
 var score = 0; // Initialize score
 
@@ -10,20 +16,21 @@ play(quiz);
 
 function play(quiz) {
 	// main game loop
-	for (var i = 0, question, answer, max=quiz.length; i < max; i++) {
+	for (var i = 0, question, answer, max=quiz.questions.length; i < max; i++) {
 
-	question = quiz[i][0];
+	question = quiz.questions[i].question;
 	answer = ask(question);
 	check(answer);
 	} // end of main game loop
+
 	gameOver();
 
 	function ask(question) {
-	return prompt(question);
+	return prompt(quiz.question + question);
 	}
 
 	function check(answer) {
-	if (answer === quiz[i][1]) { // quiz[i][1] is the ith answer
+	if (answer === quiz.questions[i].answer) { // quiz[i][1] is the ith answer
 		alert("Correct!");
 
 		score++;
