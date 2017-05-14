fc963c24aa83fd22a1c1f3b50736069c699ed333
Abel L. MBULA <bam92@debian>
2017-05-14 09:59:48 +0200

    used DOM instead of dialog boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
+/// view functions ///
+function update (element, content, klass) {
+	var p = element.firstChild || document.createElement("p");
+	p.textContent = content;
+	element.appendChild(p);
+	if (klass) {
+		p.className = klass;
+	};
+}
+
+//// dom references ///
+var $question = document.getElementById("question");
+var $score = document.getElementById("score");
+var $feedback = document.getElementById("feedback");
+
 var quiz = {
 	"name":"Super Hero Neme Quiz",
 	"description":"How many heroes can you name?",
-	"question":"What is the real name of",
+	"question":"What is the real name of ",
 	"questions": [
 		{ "question":"Superman", "answer":"Clarke Kent" },
 		{ "question":"Wonderwoman", "answer":"Dianna Prince" },
@@ -11,6 +26,8 @@ var quiz = {
 
 
 var score = 0; // Initialize score
+
+update($score, score);
 
 play(quiz);
 
@@ -26,23 +43,25 @@ function play(quiz) {
 	gameOver();
 
 	function ask(question) {
-	return prompt(quiz.question + question);
+		update($question, quiz.question + question);
+		return prompt("Enter your answer:");
 	}
 
 	function check(answer) {
 	if (answer === quiz.questions[i].answer) { // quiz[i][1] is the ith answer
-		alert("Correct!");
+		update($feedback, "Correct!", "right");
 
-		score++;
+		score++; // increase score by 1
+		update($score, score);
 	} else {
-		alert("Wrong!");
+		update($feedback, "Wrong!", "wrong");
 		}
 	}
 
 	function gameOver() {
 	// inform the player that the game has finished and tell them
 	// how many points they have scored
-	alert("Game Over, you scored " + score + " points");
+	update($question, "Game Over, you scored " + score + " points");
 	}
 }
 
